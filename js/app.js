9a88700c7092cecd25e97b15ac759848d5fc2c3a
steven peltzer <srpeltzer@yahoo.com>
2017-02-03 18:59:29 -0800

    psuedocode added

---
+++ js/app.js
@@ -0,0 +1,23 @@
+console.log('connected')
+
+//WELCOME PAGE//
+//////////////
+
+//jQuery/////
+//add directions to the div//
+//add form to the div that recieves name input//
+//add click listener to the begin button. Once button is clicked remove directions and submit name form and relocate name to gameplay page. This will also add the elements for the gameplay page (includes: canvas, new name box, live score feed, previous scores)//
+
+
+//GAMEPLAY PAGE//
+////////////////
+//canvas algorithms
+////gravity, objects, timers, live score
+
+
+//GAME OVER PAGE//
+/////////////////
+//once a player hits an object initiate this page.
+//show title 'loser!' in the div
+//show score that was obtained
+//show button that will reset the game. Upon click it will reset the canvas to begin position(eric talked about this) and add the score into the list of past sores div
